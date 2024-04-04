c13e9de10fabcf3b266f4ba70206fbe58f6c5671
edeliscoding <edelreichd@gmail.com>
2024-04-03 19:42:14 -0600

    first commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+  return <div>Redirect here to Dashboard after logging in</div>;
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // "res.cloudinary.com",
+      // "avatars.githubusercontent.com",
+      // "lh3.googleusercontent.com",
+      { protocol: "https", hostname: "**" },
+    ],
+  },
+};
 
 export default nextConfig;
